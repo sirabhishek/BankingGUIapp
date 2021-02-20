b4c652a96ace0fdb95771e901097f9f8ced5b0e3
sirabhishek <rathore.abhishek656@gmail.com>
2021-02-20 14:42:51 +0530

    commit

--- BankingGUIapp.py
+++ BankingGUIapp.py
@@ -1,9 +1,25 @@
-#imports
-from tkinter import *
-import os 
-from PIL import ImageTk, Image
+# #imports
+# from tkinter import *
+# import os 
+# from PIL import ImageTk, Image
 
-#Main Screen
-master = Tk()
-master.title('Banking App')
+# #Main Screen
+# master = Tk()
+# master.title('Banking App')
+
+import tkinter
+import _tkinter
+
+HEIGHT = 700
+WIDTH = 800
+
+root = tkinter.Tk()
+
+canvas = tkinter.Canvas(root, height = HEIGHT, width=WIDTH)
+canvas.pack()
+
+frame = tkinter.Frame(root, bg='red')
+frame.pack()
+
+root.mainloop()
 
